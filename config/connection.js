48388a6875010dfec2c3ba9744f61dbc1a7f01ee
Veli Yagci <veliyagci35@gmail.com>
2019-04-25 19:39:50 -0400

    burgerblock

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,9 @@ if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection({ 
   port: 3306,
   host: "localhost",
-  user: "lvay8mr8mx0p6vt5",
-  password: "o7sbshmr154n599i",
-  database: "krk0hqmu2ju8f580"
+  user: "root",
+  password: "",
+  database: "burgers_db"
   })
 };
 
